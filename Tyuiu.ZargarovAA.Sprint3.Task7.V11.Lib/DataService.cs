55883efb4d026caf30416059249856dd440604a6
Tyuiu.ZargarovAA@outlook.com <fairmarko@gmail.com>
2023-12-28 02:25:27 +0500

    Add project files.

---
+++ Tyuiu.ZargarovAA.Sprint3.Task7.V11.Lib/DataService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.ZargarovAA.Sprint3.Task7.V11.Lib
+{
+    public class DataService : ISprint3Task7V11
+    {
+        // Метод вычисляет и возвращает массив значений на основе заданного диапазона [startValue, stopValue].
+        public double[] GetMassFunction(int startValue, int stopValue)
+        {
+            // Объявляем массив для хранения вычисленных значений.
+            double[] valueArray;
+
+            // Вычисляем длину массива на основе заданного диапазона.
+            int len = (stopValue - startValue) + 1;
+
+            // Инициализируем массив вычисленной длиной.
+            valueArray = new double[len];
+
+            // Переменные для хранения текущих значений x и y в цикле.
+            double y;
+            int count = 0; // Счетчик для отслеживания индекса массива.
+
+            // Проходим по каждому значению x в указанном диапазоне.
+            for (int x = startValue; x <= stopValue; x++)
+            {
+                // Проверяем, равно ли x 1.
+                if (x == 1)
+                {
+                    // Если x равно 1, устанавливаем y в 0.
+                    y = 0;
+                    // Присваиваем вычисленное значение y массиву.
+                    valueArray[count] = y;
+                    // Увеличиваем счетчик.
+                    count++;
+                }
+                else
+                {
+                    // Если x не равно 1, вычисляем y на основе заданной формулы.
+                    y = Math.Round(Math.Cos(x) + (Math.Sin(x) / (2 - 2 * x)) - 4 * x, 2);
+                    // Присваиваем вычисленное значение y массиву.
+                    valueArray[count] = y;
+                    // Увеличиваем счетчик.
+                    count++;
+                }
+            }
+
+            // Возвращаем массив вычисленных значений.
+            return valueArray;
+        }
+    }
+}
---
+++ Tyuiu.ZargarovAA.Sprint3.Task7.V11.Test/DataServiceTest.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tyuiu.ZargarovAA.Sprint3.Task7.V11.Lib;
+
+namespace Tyuiu.ZargarovAA.Sprint3.Task7.V11.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // Создаем экземпляр DataService для тестирования.
+            DataService ds = new DataService();
+
+            // Устанавливаем значения начала и конца диапазона.
+            int startValue = -5;
+            int stopValue = 5;
+
+            // Вычисляем ожидаемую длину массива значений.
+            int len = stopValue - startValue + 1;
+
+            // Инициализируем ожидаемый массив значений.
+            double[] valueWaitArray;
+            valueWaitArray = new double[len];
+
+            // Устанавливаем значения в ожидаемом массиве.
+            valueWaitArray[0] = 20.36;
+            valueWaitArray[1] = 15.42;
+            valueWaitArray[2] = 10.99;
+            valueWaitArray[3] = 7.43;
+            valueWaitArray[4] = 4.33;
+            valueWaitArray[5] = 1;
+            valueWaitArray[6] = 0;
+            valueWaitArray[7] = -8.87;
+            valueWaitArray[8] = -13.03;
+            valueWaitArray[9] = -16.53;
+            valueWaitArray[10] = -19.60;
+
+            // Инициализируем массив для хранения результатов.
+            double[] res;
+            res = new double[len];
+
+            // Получаем массив значений от DataService и присваиваем его результатам.
+            res = ds.GetMassFunction(startValue, stopValue);
+
+            // Проверяем, соответствуют ли фактические результаты ожидаемым.
+            CollectionAssert.AreEqual(valueWaitArray, res);
+        }
+    }
+}
